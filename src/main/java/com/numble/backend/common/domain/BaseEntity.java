35c8b280949d0300e91a0b0880e5374d65aa9e43
idwooin <idwooin@naver.com>
2022-12-29 22:19:49 +0900

    [post] import 정리

--- src/main/java/com/numble/backend/common/domain/BaseEntity.java
+++ src/main/java/com/numble/backend/common/domain/BaseEntity.java
@@ -3,9 +3,6 @@ package com.numble.backend.common.domain;
 import java.time.LocalDateTime;
 
 import javax.persistence.EntityListeners;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
 import org.springframework.data.annotation.CreatedDate;
